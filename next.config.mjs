0e414e5db9fb8a8fe0c861be5e19fb03eef0695d
Rihards Mantejs <dexit@dyc.lv>
2025-04-10 12:07:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 pageExtensions: ['html', 'jsx', 'js', 'tsx', 'ts','mjs'],
-/*  async redirects() {
+  async redirects() {
     return [
       {
         source: '/056949325b65d64ce333ccddf5d44a9c.html',
@@ -9,7 +9,7 @@ pageExtensions: ['html', 'jsx', 'js', 'tsx', 'ts','mjs'],
         permanent: true,
       },
     ];
-  }, */
+  }, 
 };
 
 export default nextConfig;
