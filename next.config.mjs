8bc7ee141fe134ecc8ad6659ccc2460ce827321e
Rihards Mantejs <dexit@dyc.lv>
2025-04-10 11:29:47 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/d860b131acff167353f23c2412fbb2c5',
+        source: '//d860b131acff167353f23c2412fbb2c5.html',
         destination: '/d860b131acff167353f23c2412fbb2c5',
         permanent: false,
       },
