faefc3782b2919d4539ef9fede72e7b27f3379a0
Rihards Mantejs <dexit@dyc.lv>
2025-04-10 12:14:19 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ pageExtensions: ['html', 'jsx', 'js', 'tsx', 'ts','mjs'],
   async redirects() {
     return [
       {
-        source: '/056949325b65d64ce333ccddf5d44a9c.html',
+        source: '/056949325b65d64ce333ccddf5d44a9c',
         destination: '/056949325b65d64ce333ccddf5d44a9c.html',
-        permanent: true,
+        permanent: false,
       },
     ];
   }, 
