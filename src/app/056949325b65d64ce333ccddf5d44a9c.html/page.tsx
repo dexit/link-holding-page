783320c2c48447af67fa104530721e6f6bd5b7f6
Rihards Mantejs <dexit@dyc.lv>
2025-04-10 12:10:01 +0100

    Update page.tsx

--- src/app/056949325b65d64ce333ccddf5d44a9c.html/page.tsx
+++ src/app/056949325b65d64ce333ccddf5d44a9c.html/page.tsx
@@ -1,1 +1,7 @@
-twilio-domain-verification=056949325b65d64ce333ccddf5d44a9c
+export default function CustomRoutePage() {
+  return (
+<>
+   twilio-domain-verification=056949325b65d64ce333ccddf5d44a9c
+</>
+  );
+}
